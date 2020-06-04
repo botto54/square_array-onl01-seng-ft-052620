934051158c10aee0214ebddc092b880885674e33
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-06-04 19:09:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,12 @@
 def square_array(array)
+  # new_numbers = []
+  # array.each{|num| new_numbers << num**2}
+  # return new_numbers
+  
   new_numbers = []
-  array.each{|num| new_numbers << num**2}
-  return new_numbers
+  array.each do |num|
+    new_numbers << num*num
+  end
+  puts new_numbers
+  
 end
